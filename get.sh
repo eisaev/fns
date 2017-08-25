34d585fe94c2e1f33a3199119163af3989894fd7
eisaev <isaev.evgeniy@gmail.com>
2017-08-25 13:27:38 +0500

    Изменены HTTP-заголовки, т.к. похоже налоговая опомнилась и начала фильтровать запросы по ним, отвечая таймаутами. Добавлены переменные для заголовков для упрощения их дальнейшей смены на случай продолжения борьбы. Реализована генерация псевдослучайного идентификатора устройства для борьбы с фильтрацией.

--- get.sh
+++ get.sh
@@ -4,8 +4,19 @@
 PHONE="+79876543210"
 # Password from SMS
 PASS="123123"
-
+# Random device ID
+DEVID=`uuidgen | tr -d '-'`
+# DeviceID
+DEVOS="Adnroid 4.4.4"
+# Protocol version
+PROTO="2"
+# Client version
+CLIENT="1.4.1.3"
+# User agent
+UAGENT="okhttp/3.0.1"
+# Base URL
 BASE="http://proverkacheka.nalog.ru:8888"
+
 
 if [ ! -z "${1}" ] && [ ! -z "${2}" ] && [ ! -z "${3}" ]
 then
@@ -17,15 +28,15 @@ then
     # Fiscal sign (Подпись фискального документа - ФП)
     FS="${3}"
 
-    RESULT=` curl -s -G -X GET -H "Device-Id: 1234567890abcdef" -H "Device-OS: Gumanoid 42" -H "Version: 2" -H "ClientVersion: 1.4.1.3" -A "okhttp/3.0.1" -u "${PHONE}:${PASS}" "${BASE}/v1/inns/*/kkts/*/fss/${FN}/tickets/${FD}" -d "fiscalSign=${FS}" -d "sendToEmail=no"`
+    RESULT=` curl -s -G -X GET -H "Device-Id: ${DEVID}" -H "Device-OS: ${DEVOS}" -H "Version: ${PROTO}" -H "ClientVersion: ${CLIENT}" -A "${UAGENT}" -u "${PHONE}:${PASS}" "${BASE}/v1/inns/*/kkts/*/fss/${FN}/tickets/${FD}" -d "fiscalSign=${FS}" -d "sendToEmail=no"`
 
     DT=`echo "${RESULT}" | jq -r ".document.receipt.dateTime"`
     echo "${RESULT}" | jq "." > "${DT}_${FS}.json"
 else
-    RESULT=`curl -s -G -X GET -H "Device-Id: 1234567890abcdef" -H "Device-OS: Gumanoid 42" -H "Version: 2" -H "ClientVersion: 1.4.1.3" -A "okhttp/3.0.1" -u "${PHONE}:${PASS}" "${BASE}/v1/extract" -d "sendToEmail=0" -d "fileType=json"`
+    RESULT=`curl -s -G -X GET -H "Device-Id: ${DEVID}" -H "Device-OS: ${DEVOS}" -H "Version: ${PROTO}" -H "ClientVersion: ${CLIENT}" -A "${UAGENT}" -u "${PHONE}:${PASS}" "${BASE}/v1/extract" -d "sendToEmail=0" -d "fileType=json"`
 
     URL=`echo "${RESULT}" | jq -r ".url"`
-    RESULT=`curl -s -G -X GET -H "Device-Id: 1234567890abcdef" -H "Device-OS: Gumanoid 42" -H "Version: 2" -H "ClientVersion: 1.4.1.3" -A "okhttp/3.0.1" -u "${PHONE}:${PASS}" "${BASE}${URL}"`
+    RESULT=`curl -s -G -X GET -H "Device-Id: ${DEVID}" -H "Device-OS: ${DEVOS}" -H "Version: ${PROTO}" -H "ClientVersion: ${CLIENT}" -A "${UAGENT}" -u "${PHONE}:${PASS}" "${BASE}${URL}"`
 
     DT=`date -Iseconds`
     echo "${RESULT}" | jq "." > "all_${DT}.json"
